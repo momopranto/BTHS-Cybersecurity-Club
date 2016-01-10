a0382b4938023911a661fd5872ba5bef0112f056
M Amin <momopranto@users.noreply.github.com>
2016-01-09 20:14:33 -0500

    Create force-awakens.php

---
+++ 2015-16/Workshops/Week9/exercises/force-awakens.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST['password'])) {
+	if (md5($_POST['password'])=='e9b574e8a51f6e19e6ad8753860deea4') {
+            echo "flag{XXXXXXXXXXXXXXXXXXX}";
+        }
+    }
+    else {
+        echo "<h1>Welcome back, Lord Ren</h1>
+        <p>To verify your identity, please enter your secret password</p>
+        <form action='#' method='post'>
+        <input type='password' name='password' placeholder='Enter password'/>
+        <input type='submit'/>
+        </form>";
+    }
+?>
