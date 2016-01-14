735010fbcdc58fabfb6e9820e5827463cd28b723
M Amin <momopranto@users.noreply.github.com>
2016-01-14 16:38:39 -0500

    Create force-awakens-solution.py

---
+++ 2015-16/Workshops/Week9/exercises/force-awakens-solution.py
@@ -0,0 +1,17 @@
+import hashlib
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+num = '0123456789'
+
+while True:
+        pwrd = ''
+        for a in alpha:
+                for b in alpha:
+                        for c in num:
+                                for d in num:
+                                        for e in alpha:
+                                                for f in num:
+                                                        pwrd = a+b+c+d+e+f
+                                                        if hashlib.md5(pwrd).hexdigest()=='9c6b65aeedaf95394d530f685f770a0c':
+                                                                print pwrd
+                                                                break
