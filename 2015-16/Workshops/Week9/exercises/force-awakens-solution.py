4f942714ac8b1d5b13c334930118d4ab09e1e2fb
M Amin <momopranto@users.noreply.github.com>
2016-01-14 19:36:44 -0500

    Update force-awakens-solution.py

--- 2015-16/Workshops/Week9/exercises/force-awakens-solution.py
+++ 2015-16/Workshops/Week9/exercises/force-awakens-solution.py
@@ -4,14 +4,14 @@ alpha = 'abcdefghijklmnopqrstuvwxyz'
 num = '0123456789'
 
 while True:
-        pwrd = ''
-        for a in alpha:
-                for b in alpha:
-                        for c in num:
-                                for d in num:
-                                        for e in alpha:
-                                                for f in num:
-                                                        pwrd = a+b+c+d+e+f
-                                                        if hashlib.md5(pwrd).hexdigest()=='9c6b65aeedaf95394d530f685f770a0c':
-                                                                print pwrd
-                                                                break
+    pwrd = ''
+    for a in alpha:
+        for b in alpha:
+            for c in num:
+                for d in num:
+                    for e in alpha:
+                        for f in num:
+                            pwrd = a+b+c+d+e+f
+                            if hashlib.md5(pwrd).hexdigest()=='9c6b65aeedaf95394d530f685f770a0c':
+                                print pwrd
+                                break
